f6399a87820cbcb5527d12033c2192670263be5c
gsngdavid <gsngdavid@gmail.com>
2023-04-29 20:41:59 +0200

    Add errorAlert component to make some paragraphs look nice (no events found, invalid filter, ...)

--- 02-events/pages/events/[...slug].js
+++ 02-events/pages/events/[...slug].js
@@ -1,7 +1,11 @@
 import { Fragment } from "react";
 import { useRouter } from "next/router";
+
 import { getFilteredEvents } from "../../dummy-data";
 import EventList from "../../components/Events/EventList";
+import ResultsTitle from "../../components/Events/ResultsTitle";
+import Button from "../../components/UI/Button";
+import ErrorAlert from "../../components/UI/ErrorAlert";
 
 function FilteredEvents() {
     const router = useRouter();
@@ -15,16 +19,36 @@ function FilteredEvents() {
     const month = +slug[1];
 
     if(isNaN(year) || isNaN(month) || year > 2030 || year < 2010 || month < 1  || month > 12) {
-        return <p className="center">Invalid filters. Adjust your values!</p>
+        return <Fragment>
+            <ErrorAlert>
+                <p>Invalid filters. Adjust your values!</p>
+            </ErrorAlert>
+
+            <div className="center">
+                <Button link='/events'>Show All Events</Button>
+            </div>
+        </Fragment>
     }
 
     const filteredEvents = getFilteredEvents({year, month});
 
     if(!filteredEvents || filteredEvents.length === 0) {
-        return <p>No events found!</p>
+        return <Fragment>
+            <ErrorAlert>
+                <p>No events found!</p>
+            </ErrorAlert>
+            
+            <div className="center">
+                <Button link='/events'>Show All Events</Button>
+            </div>
+        </Fragment>
     }
 
+    const date = new Date(year, month - 1);
+    console.log(date);
+
     return <Fragment>
+        <ResultsTitle date={date} />
         <EventList events={filteredEvents} />
     </Fragment>
 }
--- 02-events/pages/events/[eventId].js
+++ 02-events/pages/events/[eventId].js
@@ -1,9 +1,11 @@
 import { useRouter } from 'next/router';
 import { getEventById } from '../../dummy-data';
 import { Fragment } from 'react';
+
 import EventSummary from '../../components/EventDetail/EventSummary';
 import EventLogistics from '../../components/EventDetail/EventLogistics';
 import EventContent from '../../components/EventDetail/EventContent';
+import ErrorAlert from '../../components/UI/ErrorAlert';
 
 function EventDetailPage() {
     const router = useRouter();
@@ -12,7 +14,11 @@ function EventDetailPage() {
     console.log(event);
     
     if(!event) {
-        return <h1>No events found!</h1>
+        return <Fragment>
+            <ErrorAlert>
+                <p>No event found!</p>
+            </ErrorAlert>
+        </Fragment>
     }
 
     return <Fragment>
