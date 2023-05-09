3a0526c0fbee735fd79b61bf99fdbaaf7f80f874
gsngdavid <gsngdavid@gmail.com>
2023-05-09 08:14:57 +0200

    Learn to use swr

--- 03-page-pre-rendering/pages/last-sales.js
+++ 03-page-pre-rendering/pages/last-sales.js
@@ -1,41 +1,59 @@
 import { useEffect, useState } from "react"
+import useSWR, { SWRConfig } from 'swr';
 
 
 function LastSalesPage() {
     
     const [sales, setSales] = useState();
-    const [ isLoading, setIsLoading ] = useState(false);
+    // const [ isLoading, setIsLoading ] = useState(false);
+    
+    // useEffect(() => {
+    //     setIsLoading(true);
+    //     fetch(
+    //         'https://nextjs-course-3eae2-default-rtdb.firebaseio.com/sales.json'
+    //         ).then(response => response.json()
+    //         ).then(data => {
+    //             console.log(isLoading);
+    //             let transformedSales = [];
+
+    //             for(const key in data) {
+    //                 transformedSales.push({id: key, username: data[key].username, volume: data[key].volume});
+    //             }
+            
+    //         setSales(transformedSales);
+    //     });
+    //     setIsLoading(false);
+    // }, [setIsLoading]);
+    // const fetcher = url => fetch(url).then(r => r.json())
+    const { data, error } = useSWR('https://nextjs-course-3eae2-default-rtdb.firebaseio.com/sales.json');
     
     useEffect(() => {
-        setIsLoading(true);
-        fetch(
-            'https://nextjs-course-3eae2-default-rtdb.firebaseio.com/sales.json'
-            ).then(response => response.json()
-            ).then(data => {
-                console.log(isLoading);
-                let transformedSales = [];
+        if(data) {
+            let transformedSales = [];
 
-                for(const key in data) {
-                    transformedSales.push({id: key, username: data[key].username, volume: data[key].volume});
-                }
-            
+            for(const key in data) {
+                transformedSales.push({id: key, username: data[key].username, volume: data[key].volume});
+            }
+
             setSales(transformedSales);
-        });
-        setIsLoading(false);
-    }, [setIsLoading]);
-    
+        }
+    }, [data])
 
-    if(isLoading) {
+    if(error) {
+        return <p>Failed to load data</p>;
+    }
+
+    if(!data || !sales) {
         return <p>Loading...</p>;
     }
 
-    if(!sales) {
-        return <p>No Data yet!</p>;
-    }
+    console.log(data);
+    
+    
 
     return <ul>
-        {sales.map(sale => <li key={sale.id}>{sale.username} - ${sale.volume}</li>)}
-    </ul>;
+            {sales.map(sale => <li key={sale.id}>{sale.username} - ${sale.volume}</li>)}
+        </ul>;
 }
 
 export default LastSalesPage;
