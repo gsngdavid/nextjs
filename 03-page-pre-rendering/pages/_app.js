084cdbdb0531ad5f85ca6d5e7a00a1fd354ae9f3
gsngdavid <gsngdavid@gmail.com>
2023-07-29 19:35:47 +0200

    feat: configure fetcher to throw errors if any

--- 03-page-pre-rendering/pages/_app.js
+++ 03-page-pre-rendering/pages/_app.js
@@ -2,7 +2,10 @@ import { SWRConfig } from 'swr'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <SWRConfig value={{fetcher: (resource, init) => fetch(resource, init).then(res => res.json())}}>
+  return <SWRConfig value={{fetcher: (resource, init) => fetch(resource, init).then(res => {
+    if(res.ok) return res.json();
+    throw('Something went wrong');
+  })}}>
     <Component {...pageProps} />
   </SWRConfig>
 }
