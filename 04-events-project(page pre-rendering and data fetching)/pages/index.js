82f65170e7201c484c0855ff44e30a0890ca8980
gsngdavid <gsngdavid@gmail.com>
2023-07-30 23:42:10 +0200

    feat: Fetch featured events from database

--- 04-events-project(page pre-rendering and data fetching)/pages/index.js
+++ 04-events-project(page pre-rendering and data fetching)/pages/index.js
@@ -2,7 +2,7 @@ import EventList from "../components/Events/EventList";
 import { getFeaturedEvents } from "../dummy-data";
 
 function HomePage(props) {
-  const featuredEvents = getFeaturedEvents();
+  const featuredEvents = props.events;
 
   return <div>
     <EventList events={featuredEvents} />
@@ -10,3 +10,23 @@ function HomePage(props) {
 }
 
 export default HomePage;
+
+export async function getStaticProps() {
+  const response = await fetch('https://nextjs-course-3eae2-default-rtdb.firebaseio.com/events.json');
+  const data = await response.json();
+  const featuredEvents = [];
+
+  for(let key in data) {
+    if(data[key].isFeatured) {
+      featuredEvents.push({
+        id: key,
+        ...data[key]
+      })
+    }
+  }
+  
+  return {
+    props: { events: featuredEvents },
+    revalidate: 10
+  }
+}
