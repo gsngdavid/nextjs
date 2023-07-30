87ae932c5e3af8e72e6305e7da3da59fe32d5256
gsngdavid <gsngdavid@gmail.com>
2023-07-31 00:26:59 +0200

    feat: Fetch event form database

--- 04-events-project(page pre-rendering and data fetching)/pages/events/[eventId].js
+++ 04-events-project(page pre-rendering and data fetching)/pages/events/[eventId].js
@@ -1,5 +1,3 @@
-import { useRouter } from 'next/router';
-import { getEventById } from '../../dummy-data';
 import { Fragment } from 'react';
 
 import EventSummary from '../../components/EventDetail/EventSummary';
@@ -7,11 +5,8 @@ import EventLogistics from '../../components/EventDetail/EventLogistics';
 import EventContent from '../../components/EventDetail/EventContent';
 import ErrorAlert from '../../components/UI/ErrorAlert';
 
-function EventDetailPage() {
-    const router = useRouter();
-
-    const event = getEventById(router.query.eventId);
-    console.log(event);
+function EventDetailPage(props) {
+    const { event } = props;
     
     if(!event) {
         return <Fragment>
@@ -39,3 +34,37 @@ function EventDetailPage() {
 }
 
 export default EventDetailPage;
+
+export async function getStaticProps(context) {
+    const { params } = context;
+    const response = await fetch('https://nextjs-course-3eae2-default-rtdb.firebaseio.com/events.json');
+    const data = await response.json();
+    const loadedEvent = data[params.eventId];
+
+    if(!loadedEvent) {
+        return {
+            props: {event: null}
+        }
+    }
+    
+    return {
+        props: {event : loadedEvent}
+    }
+}
+
+export async function getStaticPaths() {
+    const response = await fetch('https://nextjs-course-3eae2-default-rtdb.firebaseio.com/events.json');
+    const data = await response.json();
+    const paramsArr = [];
+
+    for(let key in data) {
+        paramsArr.push({
+            params: { eventId: key }
+        });
+    }
+
+    return {
+        paths: paramsArr,
+        fallback: true
+    }
+}
