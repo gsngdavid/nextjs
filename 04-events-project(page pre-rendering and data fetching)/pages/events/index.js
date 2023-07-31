75004a3649a225b392cfe0ea0b2931c370dc0cb0
gsngdavid <gsngdavid@gmail.com>
2023-07-31 08:51:32 +0200

    feat: use getAllEvents to get the events

--- 04-events-project(page pre-rendering and data fetching)/pages/events/index.js
+++ 04-events-project(page pre-rendering and data fetching)/pages/events/index.js
@@ -1,6 +1,6 @@
 import { Fragment } from 'react';
 import EventList from '../../components/Events/EventList';
-import { getAllEvents } from '../../dummy-data';
+import { getAllEvents } from '../../helpers/api-util';
 import EventSearch from '../../components/Events/EventsSearch';
 import { useRouter } from 'next/router';
 
@@ -21,19 +21,10 @@ function EventsPage(props) {
 export default EventsPage;
 
 export async function getStaticProps() {
-    const response = await fetch('https://nextjs-course-3eae2-default-rtdb.firebaseio.com/events.json');
-    const data = await response.json();
-    const events = [];
-
-    for(let key in data) {
-        events.push({
-            id: key,
-            ...data[key]
-        });
-    }
+    const events = await getAllEvents();
 
     return {
         props: { events },
-        revalidate: 10
+        revalidate: 120
     }
 }
