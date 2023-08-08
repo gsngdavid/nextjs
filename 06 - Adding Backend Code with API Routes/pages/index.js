305d2cf1f58a307bdbc5e672b9e2f57ea973c325
gsngdavid <gsngdavid@gmail.com>
2023-08-08 08:21:23 +0200

    feat: Add form

--- 06 - Adding Backend Code with API Routes/pages/index.js
+++ 06 - Adding Backend Code with API Routes/pages/index.js
@@ -1,5 +1,17 @@
 function HomePage() {
-  return <h1>Home page...</h1>
+  return <div>
+    <form>
+      <div>
+        <label htmlFor="email">Your email address</label><br />
+        <input type="email" id="email" />
+      </div>
+      <div>
+        <label htmlFor="message">Your feedback</label><br />
+        <textarea id="message" rows={5}></textarea>
+      </div>
+      <button type="submit">Send</button>
+    </form>
+  </div>
 }
 
 export default HomePage;
