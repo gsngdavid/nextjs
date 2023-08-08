3996338e945099fb76ecedc62454f32ac3fa7eec
gsngdavid <gsngdavid@gmail.com>
2023-08-08 08:27:55 +0200

    feat: Get form data

--- 06 - Adding Backend Code with API Routes/pages/index.js
+++ 06 - Adding Backend Code with API Routes/pages/index.js
@@ -1,13 +1,23 @@
+import { useRef } from "react";
+
 function HomePage() {
+  const emailRef = useRef();
+  const messageRef = useRef();
+
+  function submitHandler(e) {
+    e.preventDefault();
+    console.log(emailRef.current.value, messageRef.current.value);
+  }
+
   return <div>
-    <form>
+    <form onSubmit={submitHandler}>
       <div>
         <label htmlFor="email">Your email address</label><br />
-        <input type="email" id="email" />
+        <input ref={emailRef} type="email" id="email" />
       </div>
       <div>
         <label htmlFor="message">Your feedback</label><br />
-        <textarea id="message" rows={5}></textarea>
+        <textarea ref={messageRef} id="message" rows={5}></textarea>
       </div>
       <button type="submit">Send</button>
     </form>
