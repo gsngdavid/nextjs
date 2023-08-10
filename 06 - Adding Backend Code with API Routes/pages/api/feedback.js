3e2452e28ddf7d27f3ea67a08d5c979a2df5f566
gsngdavid <gsngdavid@gmail.com>
2023-08-10 08:34:45 +0200

    feat: Handle feedback get requests

--- 06 - Adding Backend Code with API Routes/pages/api/feedback.js
+++ 06 - Adding Backend Code with API Routes/pages/api/feedback.js
@@ -1,6 +1,15 @@
 import fs from 'fs';
 import path from 'path';
 
+function getDataFilePath() {
+    return path.join(process.cwd(), 'data', 'feedback.json');
+}
+
+function extractData(path) {
+    const data = fs.readFileSync(path);
+    return JSON.parse(data);
+}
+
 function feedback(req, res) {
     if(req.method === 'POST') {
         const { email, message } = req.body;
@@ -9,16 +18,17 @@ function feedback(req, res) {
             email, message
         }
         // Store data in a json file
-        const dataFilePath = path.join(process.cwd(), 'data', 'feedback.json');
-        const data = fs.readFileSync(dataFilePath);
-        const jsonData = JSON.parse(data);
-        jsonData.push(feedbackObj);
+        const dataFilePath = getDataFilePath();
+        const data = extractData(dataFilePath);
+        data.push(feedbackObj);
 
-        fs.writeFileSync(dataFilePath, JSON.stringify(jsonData));
+        fs.writeFileSync(dataFilePath, JSON.stringify(data));
         res.status(201).json({message: 'Success!', feedback: feedbackObj});
     }
     else {
-        res.status(200).json({message: 'Hello from feedback'});
+        const dataFilePath = getDataFilePath();
+        const data = extractData(dataFilePath);
+        res.status(200).json({feedback: data});
     }
 }
 
