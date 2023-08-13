c1d948b257db624f052da6f0fe32515035b61eb5
gsngdavid <gsngdavid@gmail.com>
2023-08-13 22:07:36 +0200

    feat: Pre-render feedback page

--- 06 - Adding Backend Code with API Routes/pages/feedback/index.js
+++ 06 - Adding Backend Code with API Routes/pages/feedback/index.js
@@ -1,22 +1,31 @@
-import { useEffect, useState } from "react";
+import { getDataFilePath, extractData } from "../api/feedback";
 
-function FeedbackPage() {
+function FeedbackPage(props) {
 
-    const [ feedback, setFeedback ] = useState([]);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await fetch('/api/feedback');
-            const data = await response.json();
-            setFeedback(data.feedback);
-            console.log(data.feedback);
-        }
-        fetchData();
-    }, []);
+    // const [ feedback, setFeedback ] = useState([]);
+    // 
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         const response = await fetch('/api/feedback');
+    //         const data = await response.json();
+    //         setFeedback(data.feedback);
+    //         console.log(data.feedback);
+    //     }
+    //     fetchData();
+    // }, []);
 
     return <ul>
-        {feedback.map(item => <li>{item.message}</li>)}
+        {props.feedbackItems.map(feedback => <li key={feedback.id}>{feedback.message}</li>)}
     </ul>
+}
+
+export async function getStaticProps() {
+    const path = getDataFilePath();
+    const data = extractData(path);
+
+    return {
+        props: { feedbackItems: data }
+    };
 }
 
 export default FeedbackPage;
