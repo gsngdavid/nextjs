c69a6cebc7639883641b65b51305c20044926d3c
gsngdavid <gsngdavid@gmail.com>
2023-08-13 22:26:13 +0200

    feat: Display email of selected feedback

--- 06 - Adding Backend Code with API Routes/pages/feedback/index.js
+++ 06 - Adding Backend Code with API Routes/pages/feedback/index.js
@@ -1,8 +1,9 @@
+import { Fragment, useState } from "react";
 import { getDataFilePath, extractData } from "../api/feedback";
 
 function FeedbackPage(props) {
 
-    // const [ feedback, setFeedback ] = useState([]);
+    const [ selectedFeedback, setSelectedFeedback ] = useState();
     // 
     // useEffect(() => {
     //     const fetchData = async () => {
@@ -14,9 +15,26 @@ function FeedbackPage(props) {
     //     fetchData();
     // }, []);
 
-    return <ul>
-        {props.feedbackItems.map(feedback => <li key={feedback.id}>{feedback.message}</li>)}
-    </ul>
+    function loadEmailHandler(id) {
+        fetch(`/api/${id}`)
+        .then(response => response.json())
+        .then(data => {
+            setSelectedFeedback(data.feedback);
+        })
+    }
+
+    return <Fragment>
+        {selectedFeedback && <p>{selectedFeedback.email}</p>}
+        <ul>
+            {
+                props.feedbackItems.map(feedback => 
+                <li key={feedback.id}>
+                    {feedback.message}
+                    <button onClick={loadEmailHandler.bind(null, feedback.id)}>Load email</button>
+                </li>)
+            }
+        </ul>
+    </Fragment>
 }
 
 export async function getStaticProps() {
