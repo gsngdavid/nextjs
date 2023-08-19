2dc7cef9ba4e8f746d29578540862ec005111eea
gsngdavid <gsngdavid@gmail.com>
2023-08-19 10:01:48 +0200

    feat: Store email to real database

--- 07 - Project Time API Routes (Newsletter app)/pages/api/newsletter.js
+++ 07 - Project Time API Routes (Newsletter app)/pages/api/newsletter.js
@@ -1,11 +1,17 @@
-function newsletterHandler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function newsletterHandler(req, res) {
     if(req.method === 'POST') {
         const email = req.body.email;
         if(!email || !email.includes('@')) {
             res.status(422).json({message: 'Invalid email address.'});
             return;
         }
-        console.log(email);
+
+        const client = await MongoClient.connect('DB_URL')
+        const db = client.db();
+        await db.collection('newsletter').insertOne({email});
+
         res.status(201).json({message: 'Successfully signed up!', email});
     }
 }
