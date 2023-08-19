2c61f217eceb748abf4e2875bd79eab745905f7c
gsngdavid <gsngdavid@gmail.com>
2023-08-19 10:29:31 +0200

    feat: Store comments in real database

--- 07 - Project Time API Routes (Newsletter app)/pages/api/comments/[eventId].js
+++ 07 - Project Time API Routes (Newsletter app)/pages/api/comments/[eventId].js
@@ -1,13 +1,21 @@
-function eventCommentHandler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function eventCommentHandler(req, res) {
+    const client = await MongoClient.connect('DB_URL');
+    const db = client.db();
+
     if(req.method === 'POST') {
         const { email, name, message } = req.body;
         if(!email || !name || !message || !email.includes('@') || email.trim() === '' || name.trim() === 0 ) {
             res.status(201).json({message: "Invalid input."});
+            client.close();
             return;
         }
+        const commentData = {eventId: req.query.eventId, email, name, message };
 
-        const commentData = { id: new Date().toISOString(), email, name, message };
-        console.log(commentData);
+        const result = await db.collection('comments').insertOne({...commentData});
+        commentData.id = result.insertedId;
+
         res.status(200).json({message: "Success!", comment: commentData});
     }
     else {
@@ -16,6 +24,7 @@ function eventCommentHandler(req, res) {
         ];
         res.status(200).send(JSON.stringify(comments));
     }
+    client.close();
 }
 
 export default eventCommentHandler;
