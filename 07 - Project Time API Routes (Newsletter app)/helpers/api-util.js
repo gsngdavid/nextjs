1f67b43a6dd9ee2b61d25953c8ed36a783be3a5c
gsngdavid <gsngdavid@gmail.com>
2023-08-22 07:28:56 +0200

    feat: Import MongoClient

--- 07 - Project Time API Routes (Newsletter app)/helpers/api-util.js
+++ 07 - Project Time API Routes (Newsletter app)/helpers/api-util.js
@@ -1,3 +1,6 @@
+import { MongoClient } from 'mongodb';
+
+
 export async function getAllEvents() {
   const response = await fetch('https://nextjs-course-3eae2-default-rtdb.firebaseio.com/events.json');
   const data = await response.json();
